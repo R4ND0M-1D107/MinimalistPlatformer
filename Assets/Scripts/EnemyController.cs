6a959d26b1f3308e80eb118178941f317b255e9a
R4ND0M-1D107 <meister351@gmail.com>
2019-11-13 06:10:06 +0100

    0.1.4
    - better fireball
       - animation
       - destroy on touch
    - enemy
       - scream on death
       - stop move while dying

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -4,43 +4,77 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-
- public float speed;
- private Transform target;
-
- private bool isShot;
-
- AudioSource audioSource;
- public AudioClip Scream;
-
-   public GameObject Enemy;
-
-   public Transform shotCheck;
-   public float checkRadius;
-   public LayerMask whatIsShot;
-
- // Use this for initialization
- void Start () {
-     target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
-     audioSource = GetComponent<AudioSource>();
- }
- 
- // Update is called once per frame
- void FixedUpdate () {
-     isShot = Physics2D.OverlapCircle(shotCheck.position, checkRadius, whatIsShot);     
-
- }
-
- void Update () {
-     if (isShot == true) {
-         audioSource.PlayOneShot(Scream, 0.7F);
-         Score.ScoreValue +=5;
-         if(audioSource.isPlaying){
-             Destroy(Enemy);
-         }    
-     }
     
-     transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
-     
- }
+    private Transform target;
+    public Renderer rend;
+    private bool NeverDone;
+
+    public float speed;
+
+    private bool isShot;
+
+    public GameObject Enemy;
+
+    public Transform shotCheck;
+    public float checkRadius;
+    public LayerMask whatIsShot;
+    public AudioSource audioSource;
+
+    private bool Stalk;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        audioSource = GetComponent<AudioSource>();
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+        NeverDone = true;
+        Stalk = true;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        isShot = Physics2D.OverlapCircle(shotCheck.position, checkRadius, whatIsShot);
+
+        if (target.position.x > transform.position.x)
+        {
+            //face right
+            transform.localScale = new Vector3(-0.3f, 0.3f, 1);
+        }
+        else if (target.position.x < transform.position.x)
+        {
+            //face left
+            transform.localScale = new Vector3(0.3f, 0.3f, 1);
+        }
+
+    }
+
+    void Update()
+    {
+        StalkPlayer();
+        if (isShot == true)
+        {
+            if (NeverDone == true)
+            {
+                NeverDone = false;
+                Stalk = false;
+                audioSource.Play();
+                Score.ScoreValue += 5;
+                Destroy(Enemy, audioSource.clip.length);
+            }
+
+        }
+    }
+
+    void StalkPlayer()
+    {
+        if (Stalk == true)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+    }
 }
--- Assets/Scripts/FireballDestroyer.cs
+++ Assets/Scripts/FireballDestroyer.cs
@@ -6,11 +6,37 @@ public class FireballDestroyer : MonoBehaviour
 {
     
     public GameObject fireball;
- 
+    public Transform explosionPrefab;
+
+    private bool isColliding;
+    private bool isColliding2;
+    private bool isColliding3;
+    public Transform colCheck;
+    public float checkRadius;
+    public LayerMask enemyLayer;
+    public LayerMask movableLayer;
+    public LayerMask groundLayer;
+
+
     void Start(){
         StartCoroutine (Destroy());
     }
+
+    void FixedUpdate()
+    {
+        isColliding = Physics2D.OverlapCircle(colCheck.position, checkRadius, enemyLayer);
+        isColliding2 = Physics2D.OverlapCircle(colCheck.position, checkRadius, movableLayer);
+        isColliding3 = Physics2D.OverlapCircle(colCheck.position, checkRadius, groundLayer);
+    }
  
+    void Update()
+    {
+        if(isColliding == true || isColliding2 == true || isColliding3 == true)
+        {
+            Destroy(fireball);
+        }
+    }
+    
     IEnumerator Destroy(){
         yield return new WaitForSeconds(2);
         Destroy(fireball);
